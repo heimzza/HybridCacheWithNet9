f2c609d42c4c70c7eba274b3fa749b9a3e4ff41e
Hamza Susam <hamzassm18@gmail.com>
2025-01-24 22:28:04 +0300

    fix: api => program.cs, services edited

--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -11,22 +11,23 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = builder.Configuration.GetConnectionString("Redis");
+    option.InstanceName = "FormulaOne_";
+});
+
 builder.Services.AddScoped<ICachingService, CachingService>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseSwagger();
 app.UseSwaggerUI();
