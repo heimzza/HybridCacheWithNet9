1a5ac61e1c39263d08e9dd4610fbe05fa07ec3d0
Hamza Susam <hamzassm18@gmail.com>
2025-01-25 16:13:48 +0300

    feat: api => DriversController, GetAll added

--- FormulaOne.Api/Controllers/DriversController.cs
+++ FormulaOne.Api/Controllers/DriversController.cs
@@ -11,41 +11,67 @@ public class DriversController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly ICachingService _cachingService;
-    
+
     public DriversController(IUnitOfWork unitOfWork, ICachingService cachingService)
     {
         _unitOfWork = unitOfWork;
         _cachingService = cachingService;
     }
-    
+
     [HttpGet]
-    [Route("{driverId:guid}")]
-    public async Task<IActionResult> GetDriver(Guid driverId)
+    [Route("{driverId:int}")]
+    public async Task<IActionResult> GetOne(int driverId)
     {
         var cacheKey = $"Driver_{driverId}";
-
+        
         var cachedDriver = _cachingService.Get<Driver>(cacheKey);
 
         if (cachedDriver is not null)
         {
             return Ok(cachedDriver);
         }
+        
+        var driver = await _unitOfWork.Drivers.GetByIdAsync(driverId);
 
         return NotFound($"No driver found with id {driverId}");
     }
 
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        const string cacheKey = $"Drivers";
+        
+        var drivers = _cachingService.Get<IEnumerable<Driver>>(cacheKey);
+
+        if (drivers is not null)
+        {
+            return Ok(drivers);
+        }
+        
+        drivers = await _unitOfWork.Drivers.GetAllAsync();
+
+        if (drivers is not null)
+        {
+            _cachingService.Set(cacheKey, drivers, TimeSpan.FromMinutes(5));
+            
+            return Ok(drivers);
+        }
+
+        return NotFound($"No driver found");
+    }
+
     [HttpPost]
-    public async Task<IActionResult> UpsertDriver(Driver driver)
+    public async Task<IActionResult> Upsert(Driver driver)
     {
         if (driver is null)
         {
             return BadRequest();
         }
-        
+
         await _unitOfWork.Drivers.AddAsync(driver);
 
         await _unitOfWork.SaveChangesAsync();
-        
+
         return Ok(driver);
     }
 }
